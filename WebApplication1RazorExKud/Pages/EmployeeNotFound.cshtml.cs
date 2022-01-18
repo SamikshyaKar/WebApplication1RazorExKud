cfb50036b09d03604f4e57f7c1992faa02b7a0c8
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-18 18:55:23 +0530

    -- Commit for EmployeeNotFound  Page

---
+++ WebApplication1RazorExKud/Pages/EmployeeNotFound.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1RazorExKud.Pages.Employees
+{
+    public class EmployeeNotFoundModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
