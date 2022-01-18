6eae6019dca445325242f2cac166c8c8b23d0435
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-18 16:07:27 +0530

    -- Next Commit

---
+++ WebApplication1RazorExKud/Pages/Employees/Details.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1RazorExKud.Pages.Employees
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet( int id)
+        {
+            
+        }
+    }
+}
